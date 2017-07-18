f91d2a2a3b1a33b6ce2846b482f45c4c19dfa290
Rafael M Martins <rafaelmm@br.ibm.com>
2017-07-18 17:15:48 -0300

    First commit, setting up the configuration

---
+++ src/main/java/de/embl/EmblApplication.java
@@ -0,0 +1,12 @@
+package de.embl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmblApplication.class, args);
+	}
+}
